65f0749ad6b59fc7c653646aba9ddcb029044163
Carl Pearson <carl.w.pearson@gmail.com>
2020-04-20 17:09:06 -0500

    Pin allocators to devices

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 [![Build Status](https://travis-ci.com/cwpearson/cuda-memory-management.svg?branch=master)](https://travis-ci.com/cwpearson/cuda-memory-management)
 
-C++11 allocators for CUDA memory
+C++11 allocators for CUDA memory for
+* device memory `cmm::Malloc`
+* zero-copy memory `cmm::ZeroCopy`
+* managed memory `cmm::Managed`
 
 ## Quick Start
 
@@ -34,7 +37,24 @@ int *p = allocator.allocate(10);
 allocator.deallocate(p, 10);
 ```
 
-## Notes
+Allocators can also be assigned to a specific device.
+Otherwise, they will respect calls to `cudaSetDevice()`.
 
-- [ ] bind allocators to CUDA devices (Allocators do not have to be stateless starting with C++11)
+```c++
+#include "cmm/cmm.hpp"
+
+cmm::Managed<int> allocator(3); // gpu 3
+int *p = allocator.allocate(10);
+allocator.deallocate(p, 10);
+```
+
+## Running Tests
+
+```
+make && make CTEST_OUTPUT_ON_FAILURE=1 test
+```
+
+## Roadmap
+
+- [ ] Allow passing of flags to `cudaHostAlloc` in `cmm::ZeroCopy`
 - [ ] A [low-level device allocator](https://devblogs.nvidia.com/introducing-low-level-gpu-virtual-memory-management/)
--- include/cmm/cmm.hpp
+++ include/cmm/cmm.hpp
@@ -7,6 +7,12 @@
 
 namespace cmm {
 
+#define CMM_OR_THROW(ret, exc)                                                 \
+  {                                                                            \
+    if (ret != cudaSuccess)                                                    \
+      throw exc;                                                               \
+  }
+
 namespace detail {
 
 /* round x up to nearest multiple of `up`.
@@ -16,18 +22,27 @@ inline size_t round_up(size_t x, size_t up) { return (x + up - 1) / up * up; }
 } // namespace detail
 
 template <class T> struct ZeroCopy {
+private:
+  bool set_;   // should set device
+  int device_; // the device to set
+public:
   typedef T value_type;
-  ZeroCopy() = default;
-  template <class U> constexpr ZeroCopy(const ZeroCopy<U> &) noexcept {}
+  ZeroCopy() : set_(false) {}
+  explicit ZeroCopy(int dev) : set_(true), device_(dev) {}
+
+  template <class U>
+  ZeroCopy(const ZeroCopy<U> &other)
+      : set_(other.set_), device_(other.device_) {}
   T *allocate(std::size_t n) {
     if (n > std::size_t(-1) / sizeof(T))
       throw std::bad_alloc();
-    T *p;
-    cudaError_t err = cudaHostAlloc(
-        &p, n * sizeof(T), cudaHostAllocMapped | cudaHostAllocPortable);
-    if (err != cudaSuccess) {
-      throw std::bad_alloc();
+    T *p = nullptr;
+    if (set_) {
+      CMM_OR_THROW(cudaSetDevice(device_), std::bad_alloc());
     }
+    CMM_OR_THROW(cudaHostAlloc(&p, n * sizeof(T),
+                               cudaHostAllocMapped | cudaHostAllocPortable),
+                 std::bad_alloc());
     return p;
   }
   void deallocate(T *p, std::size_t) noexcept { cudaFreeHost(p); }
@@ -41,18 +56,26 @@ bool operator!=(const ZeroCopy<T> &, const ZeroCopy<U> &) {
   return false;
 }
 
+/*
+ */
 template <class T> struct Malloc {
+private:
+  bool set_;   // should set device
+  int device_; // the device to set
+public:
   typedef T value_type;
-  Malloc() = default;
-  template <class U> constexpr Malloc(const Malloc<U> &) noexcept {}
+  Malloc() : set_(false) {}
+  explicit Malloc(int dev) : set_(true), device_(dev) {}
+  template <class U>
+  Malloc(const Malloc<U> &other) : set_(other.set_), device_(other.device_) {}
   T *allocate(std::size_t n) {
     if (n > std::size_t(-1) / sizeof(T))
       throw std::bad_alloc();
-    T *p;
-    cudaError_t err = cudaMalloc(&p, n * sizeof(T));
-    if (err != cudaSuccess) {
-      throw std::bad_alloc();
+    T *p = nullptr;
+    if (set_) {
+      CMM_OR_THROW(cudaSetDevice(device_), std::bad_alloc());
     }
+    CMM_OR_THROW(cudaMalloc(&p, n * sizeof(T)), std::bad_alloc());
     return p;
   }
   void deallocate(T *p, std::size_t) noexcept { cudaFree(p); }
@@ -66,18 +89,28 @@ bool operator!=(const Malloc<T> &, const Malloc<U> &) {
   return false;
 }
 
-template <class T> struct Managed {
+/*
+ */
+template <class T> class Managed {
+private:
+  bool set_;   // should set device
+  int device_; // the device to set
+
+public:
   typedef T value_type;
-  Managed() = default;
-  template <class U> constexpr Managed(const Managed<U> &) noexcept {}
+  Managed() : set_(false) {}
+  explicit Managed(int dev) : set_(true), device_(dev) {}
+  template <class U>
+  Managed(const Managed<U> &other) : set_(other.set_), device_(other.device_) {}
+
   T *allocate(std::size_t n) {
     if (n > std::size_t(-1) / sizeof(T))
       throw std::bad_alloc();
-    T *p;
-    cudaError_t err = cudaMallocManaged(&p, n * sizeof(T));
-    if (err != cudaSuccess) {
-      throw std::bad_alloc();
+    T *p = nullptr;
+    if (set_) {
+      CMM_OR_THROW(cudaSetDevice(device_), std::bad_alloc());
     }
+    CMM_OR_THROW(cudaMallocManaged(&p, n * sizeof(T)), std::bad_alloc());
     return p;
   }
   void deallocate(T *p, std::size_t) noexcept { cudaFree(p); }
@@ -91,4 +124,38 @@ bool operator!=(const Managed<T> &, const Managed<U> &) {
   return false;
 }
 
+#if 0
+// https://github.com/NVIDIA-developer-blog/code-samples/blob/master/posts/cuda-vmm/cuvector.h
+template <class T> struct MemMap {
+  typedef T value_type;
+
+  CUdeviceptr d_p;
+  CUmemAllocationProp prop;
+  CUmemAccessDesc accessDesc;
+
+  struct Range {
+    CUdeviceptr start;
+    size_t sz;
+  };
+
+  std::vector<CUmemGenericAllocationHandle> handles_;
+  std::vector<Range> va_ranges;
+  std::vector<size_t> handleSizes;
+
+  size_t alloc_sz;
+  size_t reserve_sz;
+  size_t chunk_sz;
+
+  MemMap() : ptr_(nullptr), size_(0) {}
+
+  // Reserves some extra space in order to speed up grow()
+  CUresult reserve(size_t new_sz);
+
+  // Actually commits num bytes of additional memory
+  CUresult grow(size_t new_sz);
+};
+#endif
+
 } // namespace cmm
+
+#undef CMM_OR_THROW
